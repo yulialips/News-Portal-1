559552710613d98c7a0c58ac1faa56f9ea9829e8
Chris <Kris_2012_98@mail.ru>
2018-04-15 23:32:01 +0300

    Slider and menu

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-﻿function ready() {
-    alert( 'DOM готов' );
-  }
+var subMenu = document.getElementById('sub-menu');
 
-  document.addEventListener("DOMContentLoaded", ready);
+document.getElementById('dropdown').onclick = function(event) {
+  var target = event.target;
+
+  if (target.id === 'dropdown') {
+
+    if (subMenu.className === 'hide') {
+      subMenu.classList.remove('hide');
+      subMenu.classList.add('show');
+    } else {
+      subMenu.classList.remove('show');
+      subMenu.classList.add('hide');
+    }
+  }
+};
+
+document.body.onclick = function(event) {
+  var target = event.target;
+
+  if (target.id !== 'dropdown' && target.id !== 'sub-menu') {
+
+    if (subMenu.className === 'show') {
+      subMenu.classList.remove('show');
+      subMenu.classList.add('hide');
+    }
+  }
+};
---
+++ slider.js
@@ -0,0 +1,11 @@
+﻿var locations = new Array("img/img1.jpg","img/img2.png","img/img3.jpg");
+
+var currentImage = 0;
+
+function nextImage() {
+  currentImage++;
+  if (currentImage === locations.length)
+    currentImage = 0;
+  document.images["picture"].src = locations[currentImage];
+  return false;
+}
