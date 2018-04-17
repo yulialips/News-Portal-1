24081b46603854b2c6553435b48b8658716f0d9b
Chris <Kris_2012_98@mail.ru>
2018-04-17 13:54:47 +0300

    backbutton

--- slider.js
+++ slider.js
@@ -1,11 +1,18 @@
-﻿var locations = new Array("img/img1.jpg","img/img2.png","img/img3.jpg");
+﻿var imgs = new array("img/img1.jpg","img/img2.png","img/img3.jpg");
 
-var currentImage = 0;
+var i = 0;
 
-function nextImage() {
-  currentImage++;
-  if (currentImage === locations.length)
-    currentImage = 0;
-  document.images["picture"].src = locations[currentImage];
+function nextimage() {
+  i++;
+  if (i === imgs.length)
+    i = 0;
+  document.images["picture"].src = imgs[i];
+  return false;
+}
+function backimage() {
+i--;
+  if (i === imgs.length)
+    i=0;
+  document.images["picture"].src = imgs[i];
   return false;
 }
